462940060d479bd5690d727bef819cfe54e76a7d
Tiago Candido <tiagocan@gmail.com>
2025-09-26 21:49:11 -0300

    feat: adicionado linearlayout

--- app/src/main/java/com/mentoriaandroid/aulacomponentesinterfaceviewbinding/LinearLayoutMainActivity.kt
+++ app/src/main/java/com/mentoriaandroid/aulacomponentesinterfaceviewbinding/LinearLayoutMainActivity.kt
@@ -1,43 +1,20 @@
 package com.mentoriaandroid.aulacomponentesinterfaceviewbinding
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mentoriaandroid.aulacomponentesinterfaceviewbinding.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    //private lateinit var binding: ActivityMainBinding
-
-    private val binding by lazy {
-        ActivityMainBinding.inflate( layoutInflater )
-    }
-
+class LinearLayoutMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //binding = ActivityMainBinding.inflate( layoutInflater )
-        setContentView( binding.root )
+        setContentView(R.layout.activity_linear_layout_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-
-      /*  with(binding){
-
-            btnClique.setOnClickListener {  }
-            btnExecutar.setOnClickListener {  }
-
-        }*/
     }
-
-
 }
