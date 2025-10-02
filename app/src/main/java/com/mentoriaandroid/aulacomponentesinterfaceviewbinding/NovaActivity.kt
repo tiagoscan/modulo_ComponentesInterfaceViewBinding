523078fa14d9e7d64abb82891c1e309153b0a922
Tiago Candido <tiagocan@gmail.com>
2025-10-02 20:10:08 -0300

    feat: aula Navegação com Toolbar

--- app/src/main/java/com/mentoriaandroid/aulacomponentesinterfaceviewbinding/NovaActivity.kt
+++ app/src/main/java/com/mentoriaandroid/aulacomponentesinterfaceviewbinding/NovaActivity.kt
@@ -1,20 +1,40 @@
 package com.mentoriaandroid.aulacomponentesinterfaceviewbinding
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mentoriaandroid.aulacomponentesinterfaceviewbinding.databinding.ActivityNovaBinding
+import com.mentoriaandroid.aulacomponentesinterfaceviewbinding.databinding.ActivityToolbarActionbarBinding
 
 class NovaActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityNovaBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_nova)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarToolbar()
+    }
+
+    private fun inicializarToolbar() {
+
+        binding.includeToolBar.clLogo.visibility = View.GONE
+        binding.includeToolBar.tbPrincipal.title = "Upload Vídeo"
+        setSupportActionBar( binding.includeToolBar.tbPrincipal )
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
